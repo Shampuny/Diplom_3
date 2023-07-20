b81fc73b5e4009bebc2c7768e2e42a0f72079ebd
Artem Seregin <vincelamarcarter@yandex.ru>
2023-07-20 17:18:43 +0300

    relative path for locators LoginPage

--- src/main/java/pageobject/LoginPage.java
+++ src/main/java/pageobject/LoginPage.java
@@ -9,8 +9,8 @@ public class LoginPage {
     private final By registrationLink = By.xpath(".//a[text()='Зарегистрироваться']");
     private final By enterHeader = By.xpath(".//h2[text()='Вход']");
     private final By enterButton = By.xpath(".//button[text()='Войти']");
-    private final By emailField = By.xpath(".//form/fieldset[1]/div/div/input");
-    private final By passwordField = By.xpath(".//form/fieldset[2]/div/div/input");
+    private final By emailField = By.xpath(".//input[@type='text']");
+    private final By passwordField = By.xpath(".//input[@type='password']");
     private final By restorePasswordLink = By.xpath(".//a[text()='Восстановить пароль']");
 
     public LoginPage(WebDriver driver) {
